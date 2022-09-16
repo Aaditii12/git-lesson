e9bcb031f599ec764401ce972f8dd845abfbec27
Aaditii12 <aaditisuryavanshi393@gmail.com>
2022-09-16 18:17:39 +0530

    file

---
+++ file.py
@@ -0,0 +1,12 @@
+Amount1 = 1200.00
+Amount2 = 1799.00
+
+Res = (Amount1 + Amount2)
+Res1 = (Amount1 / Amount2)
+Res2 = (Amount1 - Amount2)
+Res3 = (Amount1 * Amount2)
+print("Addition :", Res)
+print("Div :", Res1)
+print("Sub :", Res2)
+print("Multi :", Res3)
+
